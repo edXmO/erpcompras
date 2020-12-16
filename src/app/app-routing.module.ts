3bfe51a1f27252e4c2de9c75161c8eba254c1a3a
getafetest <getafetest@gmail.com>
2020-12-16 20:56:51 +0100

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { ListadoProductosComponent } from './productos/listado-productos/listado-productos.component';
+import { ListadoProveedoresComponent } from './proveedores/listado-proveedores/listado-proveedores.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'listado-proveedores', component: ListadoProveedoresComponent},
+  {path: 'listado-productos', component: ListadoProductosComponent},
+  {path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { NavComponent } from './nav/nav.component';
+import { ListadoProveedoresComponent } from './proveedores/listado-proveedores/listado-proveedores.component';
+import { CrearProveedorComponent } from './proveedores/crear-proveedor/crear-proveedor.component';
+import { ListadoProductosComponent } from './productos/listado-productos/listado-productos.component';
+import { CrearProductoComponent } from './productos/crear-producto/crear-producto.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NavComponent,
+    ListadoProveedoresComponent,
+    CrearProveedorComponent,
+    ListadoProductosComponent,
+    CrearProductoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
